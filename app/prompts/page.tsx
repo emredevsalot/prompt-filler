8dc2d1b51e374ee679ab765b0c3b47e60b7980d0
Emre Ergül <emredevsalot@gmail.com>
2023-07-05 18:25:10 +0300

    prompts page for all the prompts links

---
+++ app/prompts/page.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+import prompts from "@/lib/prompts";
+import Button from "@/app/components/Button";
+
+export const metadata: Metadata = {
+  title: "Prompts Page",
+  description: "...",
+};
+
+const PromptsPage = () => {
+  const content = (
+    <section className="my-2">
+      {prompts.map((prompt) => {
+        return (
+          <>
+            <p key={prompt.id}>
+              <Link href={`/prompts/${prompt.id}`}>
+                <Button>Prompt {prompt.id}</Button>
+              </Link>
+            </p>
+            <br />
+          </>
+        );
+      })}
+    </section>
+  );
+
+  return content;
+};
+
+export default PromptsPage;
