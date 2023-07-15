32a6a39f9371ebdfc4c2e1903c080aa9e0c8a0c1
Emre Ergül <emredevsalot@gmail.com>
2023-07-15 18:25:52 +0300

    fix Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className="py-6 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl">
+    <nav className="py-6 flex justify-between flex-col md:flex-row sticky top-0">
       <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
-        <Link href="/">prompt-project</Link>
+        <Link href="/">Prompt Filler</Link>
       </h1>
+      <Link href="/prompts">
+        <Button>Go to prompts</Button>
+      </Link>
     </nav>
   );
 };
