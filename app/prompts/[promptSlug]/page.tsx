b162abca56b3ce9f4916d404d7874b35291dbfeb
Emre Ergül <emredevsalot@gmail.com>
2023-07-17 18:34:16 +0300

    fix generateStaticParams

--- app/prompts/[promptSlug]/page.tsx
+++ app/prompts/[promptSlug]/page.tsx
@@ -326,7 +326,7 @@ const PromptPage = ({ params: { promptSlug } }: Params) => {
 export default PromptPage;
 
 //TODO: fix SSG (https://nextjs.org/docs/pages/building-your-application/rendering/static-site-generation)
-export async function generateStaticParams() {
+async function generateStaticParams() {
   const allPrompts = prompts;
 
   return allPrompts.map((prompt) => ({ promptSlug: prompt.slug }));
